237fd34219e8611ca30080e4051afe97950ec9d5
Ninad Rajshekhar Kavimandan <ninadkavimandan@github.com>
2020-09-03 15:52:47 +0530

    added new package

--- samplewebaspnetapp/ClassLibrary1/Class1.cs
+++ samplewebaspnetapp/ClassLibrary1/Class1.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Polly.CircuitBreaker;
 
 namespace ClassLibrary1
 {
     public class Class1
     {
+        public string NameReturner()
+        {
+            CircuitState tmp = new CircuitState();
+            Console.WriteLine(tmp.ToString());
+            return "Name";
+        }
     }
 }
